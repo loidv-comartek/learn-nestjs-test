0ed7ed97e7dcf4e08cb7b4779fbb3497632920b1
linhnv-comartek <linhnv@comartek.com>
2023-03-24 11:26:12 +0700

    get users

--- apps/nest-unit-test-pg/src/modules/users/users.controller.ts
+++ apps/nest-unit-test-pg/src/modules/users/users.controller.ts
@@ -1,20 +1,6 @@
-import { SwaggerBaseApiResponse } from '@app/common/dtos/base-api-response.dto';
-import {
-  Controller,
-  Post,
-  Body,
-  UseInterceptors,
-  ClassSerializerInterceptor,
-  HttpStatus,
-  Get,
-  Query,
-  UseGuards,
-  Patch,
-  Param,
-} from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateUserInput } from './dto/user-create-input.dto';
-import { UserOutput } from './dto/user-output.dto';
+import { FilterUserInput } from './dto/user-filter-input.dto';
+import { Controller, Body, Get, Patch, Param, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UpdateUserInput } from './dto/user-update-input.dto';
 import { UsersService } from './users.service';
 
@@ -23,12 +9,25 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({
+    summary: 'get users',
+  })
+  @Get()
+  async users(@Query() filterUser: FilterUserInput) {
+    return await this.usersService.find(filterUser);
+  }
+
+  @ApiOperation({
+    summary: 'get user by id',
+  })
   @Get(':id')
   async user(@Param('id') id: number) {
-    console.log(id);
     return await this.usersService.findOne({ id: id });
   }
 
+  @ApiOperation({
+    summary: 'update users',
+  })
   @Patch()
   async updateUser(@Body() updateUserInput: UpdateUserInput) {
     return await this.usersService.update(updateUserInput);
